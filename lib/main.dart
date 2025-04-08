2b750bd61c11d2cd6fc3d3ab6ab14ffd185e28fe
Dpi-001 <sudip9845197918@gmail.com>
2025-04-08 13:45:10 +0545

    Update code

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_lictproject/NestedContainer.dart';
 import 'package:flutter_lictproject/RowColumneg.dart';
 import 'package:flutter_lictproject/homepage.dart';
+import 'package:flutter_lictproject/image.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Rowcolumneg(),
+      home: IMageA(), //yo line ma garyem class lai call garne
+      // Rowcolumneg(),
       //Homepage(), //yo line ma garyem class lai call garne
       // Nestedcontainer()      hompage.dart file lai call garyem class
     );
