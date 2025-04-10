68533da8bb7a306a20f1a504eecbce4713de2bbb
Dpi-001 <sudip9845197918@gmail.com>
2025-04-10 12:03:56 +0545

    Updated Code

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_lictproject/MobDesgin.dart';
 import 'package:flutter_lictproject/NestedContainer.dart';
 import 'package:flutter_lictproject/RowColumneg.dart';
+import 'package:flutter_lictproject/button_example.dart';
 import 'package:flutter_lictproject/counter.dart';
 import 'package:flutter_lictproject/homepage.dart';
 import 'package:flutter_lictproject/image.dart';
+import 'package:flutter_lictproject/sizebox_example.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,9 +21,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 104, 227, 10),
+        ),
       ),
-      home: CounterApp(),
+      home: SizeboxExample(),
+      // ButtonExample(),
+      // CounterApp(),
       // Mobdesgin(),
 
       // IMageA(), //yo line ma garyem class lai call garne
