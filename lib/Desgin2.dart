d7b55fb94b301bb6d61565fe123121ce2b09aa3a
Dpi-001 <sudip9845197918@gmail.com>
2025-04-21 11:59:28 +0545

    Desgin Mobile development

--- lib/Desgin2.dart
+++ lib/Desgin2.dart
@@ -3,6 +3,27 @@ import 'package:flutter/material.dart';
 class Desgin2M extends StatelessWidget {
   const Desgin2M({super.key});
 
+  serviceCard({required IconData icon, required String text}) {
+    return Card(
+      elevation: 1,
+      shadowColor: Colors.black54,
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 19, vertical: 15),
+        child: Column(
+          spacing: 5,
+          children: [
+            Icon(icon, color: Colors.green.shade600, size: 28),
+            Text(
+              text,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,7 +78,10 @@ class Desgin2M extends StatelessWidget {
                   ],
                 ),
                 Spacer(),
-                Icon(Icons.notifications_active, color: Colors.green, size: 30),
+                Badge(
+                  label: Text("2", style: TextStyle(color: Colors.white)),
+                  child: Icon(Icons.notifications, color: Colors.red),
+                ),
               ],
             ),
 
@@ -80,6 +104,250 @@ class Desgin2M extends StatelessWidget {
               ),
             ),
             SizedBox(height: 20),
+            Row(
+              spacing: 5,
+              children: [
+                serviceCard(icon: Icons.house_outlined, text: "Hotels"),
+                serviceCard(icon: Icons.flight, text: "Flight"),
+                serviceCard(icon: Icons.bus_alert, text: "Bus"),
+                serviceCard(icon: Icons.train, text: "Train"),
+              ],
+            ),
+            SizedBox(height: 10),
+            // Banner Card
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 26, horizontal: 16),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                gradient: LinearGradient(
+                  colors: [Colors.green.shade300, Colors.green.shade600],
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    spacing: 5,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Title will be here",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 150,
+                        child: Text(
+                          "Subtitle wil be here subtitle will be here sub title will be here",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Text(
+                          "Get Started",
+                          style: TextStyle(fontWeight: FontWeight.w700),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Image.network(
+                    "https://static.vecteezy.com/system/resources/previews/045/729/459/non_2x/a-young-man-stands-with-arms-outstretched-holding-multiple-shopping-bags-he-is-looking-at-the-camera-with-a-neutral-expression-png.png",
+                    width: 170,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            SizedBox(
+              height: 49,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Text(
+                      "All",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey.shade400),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Wrap(
+                      spacing: 10,
+                      children: [
+                        Image.asset("assets/images/hotell.png"),
+                        Text(
+                          "Hotels",
+                          style: TextStyle(
+                            color: Colors.grey.shade600,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey.shade400),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      spacing: 10,
+                      children: [
+                        Image.asset("assets/images/hotell.png"),
+                        Text(
+                          "Resorts",
+                          style: TextStyle(
+                            color: Colors.grey.shade600,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 10),
+            Stack(
+              children: [
+                Container(
+                  height: 80,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        "https://www.premiercarriage.co.uk/uploads/2025/03/22/white-mercedes-g-wagon-for-hire.jpg",
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 10,
+                  right: 10,
+                  child: Icon(Icons.favorite, color: Colors.red, size: 30),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+
+            // List of items with image and text
+            Expanded(
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.only(bottom: 10),
+                    height: 80,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.shade300,
+                          blurRadius: 5,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 100,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                "https://www.premiercarriage.co.uk/uploads/2025/03/22/white-mercedes-g-wagon-for-hire.jpg",
+                              ),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 5),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Item Title"),
+                            SizedBox(height: 10),
+                            Text("Item Description"),
+
+                            Text(
+                              "Price: \$100",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.green,
+                              ),
+                            ),
+
+                            Row(
+                              children: [
+                                Icon(Icons.star, color: Colors.yellow),
+                                SizedBox(width: 5),
+                                Text("4.5"),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+
+            SizedBox(height: 10),
+            // Bottom navigation bar
+            BottomNavigationBar(
+              items: [
+                BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.search),
+                  label: "Search",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person),
+                  label: "Profile",
+                ),
+              ],
+              currentIndex: 0,
+              selectedItemColor: Colors.green,
+              unselectedItemColor: Colors.grey,
+              showUnselectedLabels: true,
+            ),
           ],
         ),
       ),
