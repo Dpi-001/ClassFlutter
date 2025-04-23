34c6751593b8ea041d17309e1a6fc6e66949c8e9
Dpi-001 <sudip9845197918@gmail.com>
2025-04-23 12:10:36 +0545

    Updated code

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_lictproject/Bmi.dart';
-import 'package:flutter_lictproject/Calculator.dart';
-import 'package:flutter_lictproject/MobDesgin.dart';
-import 'package:flutter_lictproject/NestedContainer.dart';
-import 'package:flutter_lictproject/RowColumneg.dart';
-import 'package:flutter_lictproject/button_example.dart';
-import 'package:flutter_lictproject/calcu.dart';
-import 'package:flutter_lictproject/counter.dart';
-import 'package:flutter_lictproject/formexample.dart';
-import 'package:flutter_lictproject/homepage.dart';
-import 'package:flutter_lictproject/image.dart';
-import 'package:flutter_lictproject/sizebox_example.dart';
-import 'package:flutter_lictproject/textfield.dart';
-import 'package:flutter_lictproject/textfieldcalcu.dart';
-import 'package:flutter_lictproject/textfieldform.dart';
+// import 'package:flutter_lictproject/Bmi.dart';
+// import 'package:flutter_lictproject/Calculator.dart';
+// import 'package:flutter_lictproject/Desgin2.dart';
+import 'package:flutter_lictproject/arrayfruit.dart';
+// import 'package:flutter_lictproject/first_screen.dart';
+// import 'package:flutter_lictproject/listview_builder.dart';
+// // import 'package:flutter_lictproject/MobDesgin.dart';
+// import 'package:flutter_lictproject/NestedContainer.dart';
+// import 'package:flutter_lictproject/RowColumneg.dart';
+// import 'package:flutter_lictproject/button_example.dart';
+// import 'package:flutter_lictproject/calcu.dart';
+// import 'package:flutter_lictproject/counter.dart';
+// import 'package:flutter_lictproject/formexample.dart';
+// import 'package:flutter_lictproject/homepage.dart';
+// import 'package:flutter_lictproject/image.dart';
+// import 'package:flutter_lictproject/sizebox_example.dart';
+// import 'package:flutter_lictproject/textfield.dart';
+// import 'package:flutter_lictproject/textfieldcalcu.dart';
+// import 'package:flutter_lictproject/textfieldform.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,9 +36,12 @@ class MyApp extends StatelessWidget {
           seedColor: const Color.fromARGB(255, 104, 227, 10),
         ),
       ),
-      home:
-          // Formexample(),
-          CalcuTextForm(),
+      home: Fruitarrayeg(),
+      //  FirstScreen(),
+      // CustomListView(),
+      // Desgin2M(),
+      // Formexample(),
+      // CalcuTextForm(),
       // Calceg(),
       // TextfieldExample(),
       // BmiC(), //yo line ma garyem class lai call garne
