d93a0e2a4df69f1962a63f1e14392bbcd745178f
Dpi-001 <sudip9845197918@gmail.com>
2025-04-24 15:08:20 +0545

    Array fruit

--- lib/arrayfruit.dart
+++ lib/arrayfruit.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lictproject/second_screen.dart';
 
-class Fruitarrayeg extends StatelessWidget {
-  const Fruitarrayeg({super.key});
+class Fruitarrayeg extends StatefulWidget {
+  Fruitarrayeg({super.key});
+
+  @override
+  State<Fruitarrayeg> createState() => _FruitarrayegState();
+}
+
+class _FruitarrayegState extends State<Fruitarrayeg> {
+  final GlobalKey<FormState> _addFruitFormKey = GlobalKey();
+
+  //yo line ma garyem class ko constructor banaune
+
+  List<String> fruitNames =
+      []; //yo line ma garyem array ko value haru store garne
 
   @override
   Widget build(BuildContext context) {
@@ -11,23 +24,61 @@ class Fruitarrayeg extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Enter fruit name',
-                border: OutlineInputBorder(),
+            Form(
+              key: _addFruitFormKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Enter fruit name',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a fruit name';
+                      } else if (value.length < 3) {
+                        return 'Please enter a fruit name with at least 3 characters';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (newValue) {
+                      setState(() {
+                        fruitNames.add(newValue!);
+                      });
+                    },
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (!_addFruitFormKey.currentState!.validate()) {
+                        return;
+                      }
+                      _addFruitFormKey.currentState!.save();
+                    },
+                    child: Text('Submit'),
+                  ),
+                ],
               ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a fruit name';
-                } else if (value.length < 3) {
-                  return 'Please enter a fruit name with at least 3 characters';
-                } else {
-                  return null;
-                }
-              },
             ),
-            SizedBox(height: 10),
-            ElevatedButton(onPressed: () {}, child: Text('Submit')),
+            FilledButton.tonal(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder:
+                        (context) => SecondScreeneg(fruitsNames: fruitNames),
+                  ),
+                );
+              },
+              child: Text("Go to next page"),
+            ),
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                itemBuilder: (ctx, i) => Text(fruitNames[i]),
+                itemCount: fruitNames.length,
+              ),
+            ),
           ],
         ),
       ),
