d149ffdbb030f8732691d02d7697a25df017b3b3
Dpi-001 <sudip9845197918@gmail.com>
2025-04-27 13:59:32 +0545

    Class : Todo with construtor

---
+++ lib/todo.dart
@@ -0,0 +1,11 @@
+class Todo {
+  final String Title;
+  final String Description;
+  final bool isCompleted;
+
+  Todo({
+    required this.Title,
+    required this.Description,
+    required this.isCompleted,
+  });
+}
